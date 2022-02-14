136cd47cdfe4ccdaf1f9b7a4662afe2667f3b78b
ngostradamus <roninvideostudio@gmail.com>
2022-02-14 12:56:20 -0800

    added top 5 movies functionality

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 //require('dotenv').config();
 const axios = require('axios');
 // set url as constant
-const URL = "https://imdb-api.com/en/API/MostPopularTVs/" + process.env.IMDB_TOKEN
-
+const show_URL = "https://imdb-api.com/en/API/MostPopularTVs/" + process.env.IMDB_TOKEN
+const movies_URL = "https://imdb-api.com/en/API/MostPopularMovies/" + process.env.IMDB_TOKEN
 
 client.on('ready', () => {
   console.log('Bot is ready');
@@ -14,19 +14,32 @@ client.on('ready', () => {
 client.on('message', (msg) => {
   if (msg.content === '!shows')
     axios
-      .get(URL)
+      .get(show_URL)
       .then(response => {
         msg.reply("The top 5 shows are: " + response.data.items[0]['title'] + ",   "
         + response.data.items[1]['title'] + ",   "
         + response.data.items[2]['title'] + ",   "
         + response.data.items[3]['title'] + ",   "
-        + response.data.items[4]['title'] + ",   "
+        + response.data.items[4]['title']
         );
       })
       .catch(error => {
         console.log(error);
       });
-
+    else if (msg.content === "!movies")
+      axios
+      .get(movies_URL)
+      .then(response => {
+        msg.reply("The top 5 shows are: " + response.data.items[0]['title'] + ",   "
+        + response.data.items[1]['title'] + ",   "
+        + response.data.items[2]['title'] + ",   "
+        + response.data.items[3]['title'] + ",   "
+        + response.data.items[4]['title'] 
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
 });
 
 client.login(process.env.DJS_TOKEN);
