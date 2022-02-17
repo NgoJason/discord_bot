a14f8c0ba114745549746cbb36256178d4e4115e
ngostradamus <roninvideostudio@gmail.com>
2022-02-17 10:28:56 -0800

    revert changes

--- app.js
+++ app.js
@@ -65,23 +65,5 @@ client.on('message', (msg) => {
       .catch(error => {
         console.log(error);
       });
-    else if (msg.content === "!crypto" )
-      then(response => {
-        const movieEmbed = new MessageEmbed()
-        .setColor('#FF0000')
-        .setTitle('Top 5 movies')
-        .addFields(
-          { name: "1. " + response.data.items[0]['fullTitle'], value: 'IMDB Rating: ' + response.data.items[0]['imDbRating'] },
-          { name: "2. " +response.data.items[1]['fullTitle'], value: 'IMDB Rating: ' + response.data.items[1]['imDbRating'] },
-          { name: "3. " +response.data.items[2]['fullTitle'], value: 'IMDB Rating: ' + response.data.items[2]['imDbRating'] },
-          { name: "4. " +response.data.items[3]['fullTitle'], value: 'IMDB Rating: ' + response.data.items[3]['imDbRating'] },
-          { name: "5. " +response.data.items[4]['fullTitle'], value: 'IMDB Rating: ' + response.data.items[4]['imDbRating'] },
-        )
-        .setTimestamp()
-        msg.reply("hi");
-      })
-      .catch(error => {
-        console.log(error);
-      });
 });
 client.login(process.env.DJS_TOKEN);
